7a208c1c1920eb1480489b6f4294347b66496302
rubenhamza1 <90246098+rubenhamza1@users.noreply.github.com>
2022-03-29 09:58:05 +0200

    Add files via upload

---
+++ NasljedivanjeGeometrijskoTijelo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NasljedivanjeGeometrijskoTijelo
+{
+    class GeometrijskoTijelo
+    {
+        double volumen, oplosje;
+        static int brojacTijela;
+
+        public double Volumen { get => volumen; set => volumen = value; }
+        public double Oplosje { get => oplosje; set => oplosje = value; }
+        public static int BrojacTijela { get => brojacTijela; set => brojacTijela = value; }
+    }
+    class Kugla : GeometrijskoTijelo
+    {
+
+    }
+    class Kvadar : GeometrijskoTijelo
+    {
+
+    }
+    class Kocka : Kvadar
+    {
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kugla x = new Kugla();
+            Kvadar y = new Kvadar();
+            Kocka z = new Kocka();
+
+        }
+    }
+}
